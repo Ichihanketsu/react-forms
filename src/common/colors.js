939db91efe8f954185b506cfbfe619153697ded1
NicholasShamrock <135591824+NicholasShamrock@users.noreply.github.com>
2023-07-07 18:34:57 +0200

    feat: add colors

---
+++ src/common/colors.js
@@ -0,0 +1,8 @@
+export const PRIMARY_COLOR = "";
+export const SECONDARY_COLOR = "";
+export const TERTIARY_COLOR = "";
+export const DISABLED_COLOR = "#4E4E50";
+export const ERROR_COLOR = "#950740";
+export const TEXT_COLOR = "#FFFFFF";
+export const HIGHLIGHT_COLOR = "#C3073F";
+export const BACKGROUND_COLOR = "#1A1A1D";
